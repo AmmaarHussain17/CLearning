2b68433844e66e4b0dde3582328d80773b5867bc
Mohammed Ammaar Hussain <164994848+AmmaarHussain17@users.noreply.github.com>
2025-02-06 14:38:43 +0530

    23.CheckWhetherCharacterEnteredIsLowerCaseOrUpperCaseOrNotAndCheckWeatherAnotherCharacterIsSpecialSymbolOrNotUsingConditionalOperators.c
    Chapter 4 : More Complex Decision Making

--- 23.CheckWhetherCharacterEnteredIsLowerCaseOrUpperCaseOrNotAndCheckWeatherAnotherCharacterIsSpecialSymbolOrNotUsingConditionalOperators.c
+++ 23.CheckWhetherCharacterEnteredIsLowerCaseOrUpperCaseOrNotAndCheckWeatherAnotherCharacterIsSpecialSymbolOrNotUsingConditionalOperators.c
@@ -13,7 +13,7 @@ int main()
     char character2,a;
     printf("\n\nNow Enter a Special Symbol of your choice :");
     //scanf("%c",&a);
-    scanf("%*c%c",&character2);
+    scanf("%*c%c",&character2); // By writing asterisk * before %c are telling to ignore the before character which is being entered.
 
      (character2 >= 0 && character2 <= 47 || character2 >= 58 && character2 <= 64 || character2 >= 91 && character2 <= 96 || character2 >= 123 && character2 <= 127 ? printf("Special Symbol") : printf("Not a Special Symbol"));
 
